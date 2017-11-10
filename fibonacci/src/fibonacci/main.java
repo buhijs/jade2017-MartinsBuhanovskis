d04fe818e89ef077e643d649bac307184b0868bf
Buhanovskis Mārtiņš <m7_buhano_m@VEA.LV>
2017-11-10 11:03:08 +0200

    first commit

---
+++ fibonacci/src/fibonacci/main.java
@@ -0,0 +1,23 @@
+package fibonacci;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class main {
+	// fibonacci class
+	public static void main(String[] args) {
+		
+		// number input
+		int number = new Scanner(System.in).nextInt();
+		
+		long[] series = new long[number];
+		series[0] = 0;
+		series[1] = 1;
+		
+		for (int i = 2; i < number; i++) {
+		      series[i] = series[i - 1] + series[i - 2];
+		}
+		System.out.print(Arrays.toString(series));
+	}
+	
+}
